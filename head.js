10ce9b35916e0300636f70f6375f00589ed77524
vbabison <bobby@Babisons-Mac-mini.local>
2021-02-01 16:40:16 -0500

    Implement-head

--- head.js
+++ head.js
@@ -1,3 +1,7 @@
+const head = function(arr) {
+  return arr[0];
+};
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -5,9 +9,13 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
+assertEqual(head([]), 5);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+/*
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(1, "1");
 assertEqual(1, 3);
 assertEqual('Hello', "Hello");
+*/
