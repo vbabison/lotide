cbdbe988c745a3be9e442ac2530797602cc9aa66
vbabison <bobby@Babisons-Mac-mini.local>
2021-02-02 19:08:33 -0500

    Comparing two arrays

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let fakeValue = true
+  arr1.forEach((e1) => arr2.forEach((e2) => 
+    {if (e1 === e2) {
+      fakeValue = true
+    } else {
+      fakeValue = false
+    }
+   }
+ ))
+ return fakeValue
+}
+
+/*Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
+*/
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
