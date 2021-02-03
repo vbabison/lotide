17539f63fbd13415c1b59ac520a6154a4e610806
vbabison <bobby@Babisons-Mac-mini.local>
2021-02-02 19:12:01 -0500

    Lint check

--- eqArrays.js
+++ eqArrays.js
@@ -7,25 +7,25 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  let fakeValue = true
-  arr1.forEach((e1) => arr2.forEach((e2) => 
-    {if (e1 === e2) {
-      fakeValue = true
+  let fakeValue = true;
+  arr1.forEach((e1) => arr2.forEach((e2) =>{
+    if (e1 === e2) {
+      fakeValue = true;
     } else {
-      fakeValue = false
+      fakeValue = false;
     }
-   }
- ))
- return fakeValue
-}
+  }
+  ));
+  return fakeValue;
+};
 
 /*Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 */
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
