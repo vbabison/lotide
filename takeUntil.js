60a3cae333ff537f8b2a78561800ee71d1a06899
vbabison <bobby@Babisons-Mac-mini.local>
2021-02-04 15:16:51 -0500

    return sliced array

---
+++ takeUntil.js
@@ -0,0 +1,50 @@
+const eqArrays = function(arr1, arr2) {
+  let fakeValue = true;
+  arr1.forEach((e1) => arr2.forEach((e2) =>{
+    if (e1 === e2) {
+      fakeValue = true;
+    } else {
+      fakeValue = false;
+    }
+  }));
+  return fakeValue;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const takeUntil = function(array, callback) {
+  let arr = [];
+  for (const e of array) {
+    if (!callback(e)) {
+      arr.push(e);
+    } else {
+      return arr;
+    }
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+/*
+Expected Output
+[ 1, 2, 5, 7, 2 ]
+---
+[ 'I\'ve', 'been', 'to', 'Hollywood' ]
+*/
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
